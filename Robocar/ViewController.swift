d03a0980996c02a235da06eccf23e020653640d7
Hendrik Louw <hflouw@gmail.com>
2016-03-24 19:51:21 +0200

    better controls

--- Robocar/ViewController.swift
+++ Robocar/ViewController.swift
@@ -10,9 +10,131 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBAction func forwardButtonClicked(sender: UIButton) {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/forward")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+    }
+   
+    @IBAction func forwardButtonExit(sender: UIButton) {
+        stopAcceleration()
+    }
+    
+    @IBAction func backwardButtonClicked(sender: UIButton) {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/backward")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func backwardButtonExit(sender: AnyObject) {
+        stopAcceleration()
+    }
+    
+    @IBAction func leftButtonClicked(sender: UIButton) {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/left")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func leftButtonExit(sender: AnyObject) {
+        stopTurning()
+    }
+    
+    @IBAction func rightButtonClicked(sender: UIButton) {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/right")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+    }
+    
+    
+    @IBAction func rightButtonExit(sender: AnyObject) {
+        stopTurning()
+    }
+    
+
+    @IBAction func stopButtonClicked(sender: UIButton) {
+      stop()
+    }
+    
+    
+    
+    func stop() {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/stop_acceleration")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+        
+    }
+    
+    
+    func stopAcceleration() {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/stop_acceleration")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+        
+    }
+
+
+    func stopTurning() {
+        let url = NSURL(string: "http://172.20.10.2:3000/api/robots/dc_motor_bot/commands/stop_turning")!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data,response,error in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
